38ddca434db5343f1af47f13e60a91ee3538feb7
hckang80 <hckang80@gmail.com>
2025-04-10 00:28:22 +0900

    feat: 검색시 공백 입력 방지

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   });
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.currentTarget.value);
+    setSearchQuery(e.currentTarget.value.trim());
   };
 
   return (
