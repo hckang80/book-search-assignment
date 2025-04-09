f42c0fc3f933fdd704bcb8ecec81a49100d24183
hckang80 <hckang80@gmail.com>
2025-04-10 00:05:30 +0900

    feat(fetchBookSearch): fetch API 선언

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,1 +1,8 @@
-export * from './axiosInstance';
+import { axiosInstance } from './axiosInstance';
+import type { BookInstance, BookSearchParams } from '../types';
+
+export const fetchBookSearch = async (params: BookSearchParams) => {
+  const { data } = await axiosInstance.get<BookInstance>(`/search/book`, { params });
+
+  return data;
+};
