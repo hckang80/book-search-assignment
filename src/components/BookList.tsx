42b62463582cba5a95ed5225305ebc5b44855807
hckang80 <hckang80@gmail.com>
2025-04-11 01:18:29 +0900

    feat(BookList): 데이터 바인딩

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -3,6 +3,8 @@ import { useInfiniteBookSearch } from '../hooks';
 import { Accordion } from 'radix-ui';
 import type { BookDocument, BookSearchParams } from '../types';
 import { InfiniteScrollTrigger } from './shared';
+import { Button, Theme } from '@radix-ui/themes';
+import { ChevronDown, ChevronUp } from 'lucide-react';
 
 interface BookListProps {
   query: string;
@@ -51,34 +53,80 @@ const BookList = ({ query, params }: BookListProps) => {
   return (
     <>
       <Accordion.Root type="single" collapsible>
-        {data?.pages.map((page, pageIndex) => (
-          <div key={pageIndex} style={{ display: 'contents' }}>
-            {page.documents.map((book) => (
-              <Accordion.Item key={book.title + book.isbn} value={book.title + book.isbn}>
-                <div>
+        <Theme>
+          {data?.pages.map((page, pageIndex) => (
+            <div key={pageIndex} style={{ display: 'contents' }}>
+              {page.documents.map((book) => (
+                <Accordion.Item key={book.title + book.isbn} value={book.title + book.isbn}>
                   <div>
-                    <img
-                      src={book.thumbnail}
-                      alt=""
-                      width="120"
-                      height="174"
-                      loading={pageIndex ? 'lazy' : 'eager'}
-                      decoding={pageIndex ? 'async' : 'sync'}
-                    />
+                    <div>
+                      <img
+                        src={book.thumbnail}
+                        alt=""
+                        width="48"
+                        height="70"
+                        loading={pageIndex ? 'lazy' : 'eager'}
+                        decoding={pageIndex ? 'async' : 'sync'}
+                      />
+                    </div>
+                    <div>
+                      <span>{book.title}</span>
+                      <span>{book.authors}</span>
+                    </div>
+                    <div>{book.sale_price}</div>
+                    <div>
+                      <Button size="4" asChild>
+                        <a href={book.url} target="_blank" rel="noopener noreferrer">
+                          구매하기
+                        </a>
+                      </Button>
+                      <Accordion.Trigger asChild>
+                        <Button size="4" color="gray" variant="soft">
+                          상세보기
+                          <ChevronDown size={18} />
+                        </Button>
+                      </Accordion.Trigger>
+                    </div>
                   </div>
-                  <Accordion.Trigger>상세보기</Accordion.Trigger>
-                </div>
-                <Accordion.Content>
-                  <button onClick={() => toggleLikedBook(book)}>
-                    <img src={book.thumbnail} alt={book.title} />
-                  </button>
-                  {book.title}
-                  <Accordion.Trigger>상세보기</Accordion.Trigger>
-                </Accordion.Content>
-              </Accordion.Item>
-            ))}
-          </div>
-        ))}
+                  <Accordion.Content>
+                    <div>
+                      <button onClick={() => toggleLikedBook(book)}>
+                        <img src={book.thumbnail} alt={book.title} />
+                      </button>
+                    </div>
+                    <div>
+                      <div>
+                        <span>{book.title}</span>
+                        <span>{book.authors}</span>
+                      </div>
+                      <dl>
+                        <dt>책 소개</dt>
+                        <dd>{book.contents}</dd>
+                      </dl>
+                    </div>
+                    <div>
+                      <Accordion.Trigger asChild>
+                        <Button size="4" color="gray" variant="soft">
+                          상세보기
+                          <ChevronUp size={18} />
+                        </Button>
+                      </Accordion.Trigger>
+                      <div>
+                        <span>{book.price}</span>
+                        <span>{book.sale_price}</span>
+                      </div>
+                      <Button size="4" asChild>
+                        <a href={book.url} target="_blank" rel="noopener noreferrer">
+                          구매하기
+                        </a>
+                      </Button>
+                    </div>
+                  </Accordion.Content>
+                </Accordion.Item>
+              ))}
+            </div>
+          ))}
+        </Theme>
       </Accordion.Root>
       <InfiniteScrollTrigger
         onIntersect={() => {
