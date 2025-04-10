37d15d46ce587ea3bf1168af0f6d459fa6c00f10
hckang80 <hckang80@gmail.com>
2025-04-10 20:01:20 +0900

    style: 검색폼 스타일링

---
+++ src/components/SearchBar.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const form = style({});
+
+export const wrapper = style({
+  gap: 11,
+  height: 50,
+  paddingInline: 10,
+  border: 0,
+  boxShadow: 'none',
+  borderRadius: 100
+});
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,6 @@
 import { TextField } from '@radix-ui/themes';
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
+import { wrapper } from './SearchBar.css.ts';
 
 interface SearchBarProps {
   value: string;
@@ -16,9 +17,10 @@ const SearchBar = ({ value, onChange, onSubmit, onFocus }: SearchBarProps) => {
         onChange={onChange}
         onFocus={onFocus}
         placeholder="검색어를 입력하세요"
+        className={`palette-light-gray caption ${wrapper}`}
       >
         <TextField.Slot>
-          <MagnifyingGlassIcon height="16" width="16" />
+          <MagnifyingGlassIcon height="30" width="30" color="var(--text-primary)" />
         </TextField.Slot>
       </TextField.Root>
     </form>
