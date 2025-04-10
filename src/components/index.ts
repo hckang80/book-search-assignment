2882586fb89c7bb78b32103ae134750df95da2b9
hckang80 <hckang80@gmail.com>
2025-04-10 14:38:15 +0900

    refactor: 무한스크롤 컴포넌트 별도 분리

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export * from './shared';
 export { default as BookList } from './BookList';
 export { default as DetailSearch } from './DetailSearch';
 export { default as LikedList } from './LikedList';
