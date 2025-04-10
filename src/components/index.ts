8bedb68784667f68ed561ba9ab46a9fb1f7cecf5
hckang80 <hckang80@gmail.com>
2025-04-10 14:23:58 +0900

    feat: 찜목록 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as BookList } from './BookList';
 export { default as DetailSearch } from './DetailSearch';
+export { default as LikedList } from './LikedList';
 export { default as SearchHistory } from './SearchHistory';
