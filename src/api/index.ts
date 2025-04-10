1f1d5aa58001d5787207e5e334f129ea8148f1f7
hckang80 <hckang80@gmail.com>
2025-04-10 14:17:34 +0900

    refactor: 컴포넌트 관심사 분리

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,28 @@
 import { axiosInstance } from './axiosInstance';
-import type { BookInstance, BookSearchParams } from '../types';
+import type { BookDocument, BookInstance, BookSearchParams } from '../types';
+import { PAGE_SIZE } from '../lib/constant';
 
 export const fetchBookSearch = async (params: BookSearchParams) => {
   const { data } = await axiosInstance.get<BookInstance>(`/search/book`, { params });
 
   return data;
+};
+
+export const fetchLikedBooks = async (allBooks: BookDocument[], pageParam: number) => {
+  const { length: total_count } = allBooks;
+  const pageable_count = total_count;
+  const startIndex = (pageParam - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  const pageItems = allBooks.slice(startIndex, endIndex);
+
+  const response: BookInstance = {
+    documents: pageItems,
+    meta: {
+      is_end: endIndex >= total_count,
+      pageable_count,
+      total_count
+    }
+  };
+
+  return Promise.resolve(response);
 };
--- src/hooks/useInfiniteLikedBooks.ts
+++ src/hooks/useInfiniteLikedBooks.ts
@@ -1,28 +1,11 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
-import type { BookDocument, BookInstance } from '../types';
-import { PAGE_SIZE } from '../lib/constant';
+import type { BookDocument } from '../types';
+import { fetchLikedBooks } from '../api';
 
 export const useInfiniteLikedBooks = (allBooks: BookDocument[]) => {
   return useInfiniteQuery({
     queryKey: ['likedBooks'],
-    queryFn: ({ pageParam }) => {
-      const { length: total_count } = allBooks;
-      const pageable_count = total_count;
-      const startIndex = (pageParam - 1) * PAGE_SIZE;
-      const endIndex = startIndex + PAGE_SIZE;
-      const pageItems = allBooks.slice(startIndex, endIndex);
-
-      const response: BookInstance = {
-        documents: pageItems,
-        meta: {
-          is_end: endIndex >= total_count,
-          pageable_count,
-          total_count
-        }
-      };
-
-      return Promise.resolve(response);
-    },
+    queryFn: ({ pageParam }) => fetchLikedBooks(allBooks, pageParam),
     getNextPageParam: (lastPage, allPages) => {
       const { is_end: isListEnd } = lastPage.meta;
       return isListEnd ? undefined : allPages.length + 1;
