29fd82d3a02e9e867145f8377d7aa2bafcb8642a
hckang80 <hckang80@gmail.com>
2025-04-11 23:06:58 +0900

    feat: 결과 없음용 컴포넌트 추가

---
+++ src/components/shared/NoData.tsx
@@ -0,0 +1,11 @@
+import ICON_BOOK from '../../assets/icon_book.svg';
+import { noData, noDataIcon } from './NoData.css';
+
+export const NoData = ({ message = '검색된 결과가 없습니다.' }: { message?: string }) => {
+  return (
+    <div className={noData}>
+      <img className={noDataIcon} src={ICON_BOOK} alt="" />
+      <p className="text-secondary caption">{message}</p>
+    </div>
+  );
+};
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,1 +1,2 @@
 export * from './InfiniteScrollTrigger';
+export * from './NoData';
