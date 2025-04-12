ec1d9d3b810a5cb8a6a19fcf671730d9ad58edf6
hckang80 <hckang80@gmail.com>
2025-04-12 19:09:04 +0900

    chore: 미사용 패키지 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin(), tsconfigPaths()],
+  plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: {
       src: '/src'
